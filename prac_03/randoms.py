39bcca8611346c9ed86e8bf8668397bbc82061c7
Zar Chi Oo <zarchioo@my.jcu.edu.au>
2022-11-27 19:44:50 +0630

    Attempt

---
+++ prac_03/randoms.py
@@ -0,0 +1,12 @@
+#The answer was 20.
+#The smallest number could be 5 and the largest would be 20
+
+#The answer was 9.
+# The smallest number could be 3 and the largest could be 9
+
+
+#The answer was 4.701374208501132.
+# The smallest number would be 2.5 and the largest is 5.5
+import random
+number=random.randint(1,100)
+print(number)
