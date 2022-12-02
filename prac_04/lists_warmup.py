9042883ef893682765681a4d627cb25eb2990902
Zar Chi Oo <zarchioo@my.jcu.edu.au>
2022-12-03 00:14:30 +0630

    done

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,23 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+#3
+#2
+#1
+#3,1,4,1,5,9
+#True
+#False
+#False
+#[3,1,4,1,5,9,2,6,5,3]
+
+# numbers[0]
+# numbers[-1]
+# print(numbers[3])
+# print(numbers[:-1])
+# numbers[3:4]
+# 5 in numbers
+# print(7 in numbers)
+# print("3" in numbers)
+# print(numbers + [6, 5, 3])
+numbers[0]="ten"
+numbers[-1]=1
+numbers[:2]
+9 in numbers
