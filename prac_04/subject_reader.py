ff837a5dd7c7c8fa20b68b4d90a38af74f0eb0bf
Zar Chi Oo <zarchioo@my.jcu.edu.au>
2022-12-04 18:53:13 +0630

    unedited file

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -28,7 +28,7 @@ def get_data():
 
 main()
 
-CP1401,Ada Lovelace,192
-CP1404,Alan Turing,98
-CP4321,Bill Gates,676
-CP1234,Steve Jobs,123
+# CP1401,Ada Lovelace,192
+# CP1404,Alan Turing,98
+# CP4321,Bill Gates,676
+# CP1234,Steve Jobs,123
