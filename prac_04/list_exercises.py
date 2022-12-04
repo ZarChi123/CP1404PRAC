d9ef71f8ac781edf8f6ac7f59c537c5c4ef314d4
Zar Chi Oo <zarchioo@my.jcu.edu.au>
2022-12-04 20:46:42 +0630

    done

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+List exercises
+"""
+numbers=[]
+for i in range(5):
+    number=int(input("Number:"))
+    numbers.append(number)
+print(f"The first number is",numbers[0])
+print("The last number is",numbers[-1])
+print("The smallest number is",min(numbers))
+print("The largest number is",max(numbers))
+print("The average number is",sum(numbers)/len(numbers))
+
+"""Woefully inadequate security checker"""
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username=input("Enter username:")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
