486a6bc6e79f723f5fca967477316131af064ca9
Zar Chi Oo <zarchioo@my.jcu.edu.au>
2022-12-11 21:06:24 +0630

    done adding loops

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,13 +1,22 @@
 """Edited code"""
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales",
-                "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria",
-                "TAS": "Tasmania"}
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
 
-short_state = input("Enter short state: ").upper()
-while short_state != "":
-    if short_state in CODE_TO_NAME:
-        print(short_state, "is", CODE_TO_NAME[short_state])
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ").upper()
+    state_code = input("Enter short state: ")
+
+print()
+for state in CODE_TO_NAME:
+    print("{:3} is {}".format(state, CODE_TO_NAME[state]))
