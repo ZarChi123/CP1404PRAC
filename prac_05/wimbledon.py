9cb12de546f174a0e2f5dc2ef846956803305146
Zar Chi Oo <zarchioo@my.jcu.edu.au>
2022-12-11 22:55:11 +0630

    unfinished version (need lecturer's help)

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,23 @@
+"""Wimbledon program"""
+FILENAME = "wimbledon.csv"
+
+
+def main(champion_to_count=None):
+    """Reads data file and print the details about Wimbledon and it's champions and countries """
+    get_records(FILENAME)
+    process_records(records)
+    display_results(champion_to_count, countries)
+
+
+
+def process_records(records):
+    pass
+
+
+def display_results(champion_to_count, countries):
+    pass
+
+
+def get_records(FILENAME):
+
+"""Ask lecture's help and redo this program"""
